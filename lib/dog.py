fdbdb85c56ea0a2b123f90062507afec3340239a
Sam <samuel.waters@gmail.com>
2022-12-19 12:15:44 -0700

    half

--- lib/dog.py
+++ lib/dog.py
@@ -9,4 +9,13 @@ class Dog:
         self.name = name
         self.breed = breed
 
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute("CREATE TABLE IF NOT EXISTS dogs(id INTEGER PRIMARY KEY, name TEXT, breed TEXT)")
     
+    @classmethod
+    def drop_table(self):
+        CURSOR.execute("DROP TABLE IF EXISTS dogs")
+
+    def save(self):
+        CURSOR.execute("INSERT INTO dogs(name, breed) VALUES (?, ?)", (self.name, self.breed))
